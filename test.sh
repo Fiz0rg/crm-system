3be67534bf7e27d50fc3405e0995f6e327fd76d9
Fiz0rg <roma_03-96@mail.ru>
2022-11-24 20:46:38 +0100

    bash script

---
+++ test.sh
@@ -0,0 +1,8 @@
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+python3 manage.py createsuperuser --email='admin@srm.ru' --phone='admin'
+
+# Uncomment before production
+# rm test.sh
